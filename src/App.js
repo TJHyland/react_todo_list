9a394ad40e4c01806c82ce68aacd77558055a4be
Tyson Hyland <tyhyland@hotmail.com>
2019-06-12 20:50:58 -0600

    end of lecture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, {Component } from 'react';
+import List from './components/todos/List';
+import TodoForm from './components/todos/TodoForm';
 
-function App() {
-  return (
-    <div className="App">
-   
-    </div>
-  );
+class App extends Component {
+  state = { 
+    todos: [
+      { id: 1, name: 'Learn Rails', complete: true },
+      { id: 2, name: 'Learn React', complete: false },
+      { id: 3, name: 'Learn Router', complete: false },
+     ]
+  }
+
+  // renderTodos = () => {
+  //   const { todos } = this.state
+  //   return todos.map( todo => {
+  //     return (
+  //       <li key={todo.id}>{todo.name}</li>
+  //     )
+  //   })
+  // }
+
+  render() {
+    const { todos } = this.state
+    return (
+      <div>
+        <TodoForm />
+        <List name='Todo List' items={todos}/>
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/todos/TodoForm.js
@@ -0,0 +1,31 @@
+import React, {Component } from 'react';
+
+class TodoForm extends Component {
+
+  state = { name: '' }
+
+  handleChange = (e) => {
+    const { name, value } = e.target
+    this.setState({ [name]: value })
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+  }
+
+  render() {
+    return(
+      <form onSubmit={this.handleSubmit}>
+        <input
+          required 
+          placeholder='add a todo here'
+          name="name"
+          value={this.state.anme}
+          onChange={this.handleChange}
+        />
+      </form>
+    )
+  }
+}
+
+export default TodoForm
